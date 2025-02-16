3eee92d05cdd783ab1b1c76a9d23996fc5ef6cac
eliseu <eliseudaniel29@gmail.com>
2025-02-15 21:59:55 -0300

    create project

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\{
+    AntropometriaController,
+    CalendarioController,
+    DietasController,
+    PacientesController,
+    TreinosController,
+    UsuariosController,
+};
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('usuarios', UsuariosController::class);
+    Route::apiResource('pacientes', PacientesController::class);
+    Route::apiResource('antropometria', AntropometriaController::class);
+    Route::apiResource('dietas', DietasController::class);
+    Route::apiResource('treinos', TreinosController::class);
+    Route::apiResource('calendario', CalendarioController::class);
+});
