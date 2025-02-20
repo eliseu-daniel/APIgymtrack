36a5f4d0be3a0f3f53aca17b2037bd677a798568
eliseu <eliseudaniel29@gmail.com>
2025-02-20 10:57:02 -0300

    init falte pacientes

--- app/Models/Pacientes.php
+++ app/Models/Pacientes.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pacientes extends Model
 {
-    //
+    protected $table = 'Pacientes';
+
+    protected $primaryKey = 'idPaciente';
+
+    public $timestamps = false; // usado quando nao tem no banco as tabelas de 'criado'e 'modificado'
+
+    protected $fillable = [
+        'idUsuario',
+        'nomePaciente',
+        'emailPaciente',
+        'telefonePaciente',
+        'nascimentoPaciente',
+        'planoAcompanhamento',
+        'inicioAcompanhamento',
+        'fimAcompanhamento',
+        'sexoPaciente',
+        'pagamento',
+        'alergias'
+    ];
+
+    
 }
