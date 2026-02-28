7ea4562a997ee8f7ee2725b96e9128e9e750a96f
eliseu <eliseudaniel29@gmail.com>
2026-02-28 09:54:45 -0300

    fix: create patient default for seed

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Patient;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class PatientSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Patient::factory()->count(10)->create();
+        Patient::create([
+            "name" =>"Talita Bueno",
+            "email" => "talita@gmail.com",
+            "phone" => "12341234123",
+            "birth_date" => "2001-05-10",
+            "gender"=>"female",
+            "allergies"=>"nenhuma"
+        ]);
+        \App\Models\Patient::factory()->count(9)->create();
     }
 }
