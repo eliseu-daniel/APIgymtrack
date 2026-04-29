f26b918273670c4fc04600cb191bc30aa1c677b9
eliseu <eliseudaniel29@gmail.com>
2026-04-28 22:24:14 -0300

    feat: populate FoodSeeder with a comprehensive list of food items

--- database/seeders/FoodSeeder.php
+++ database/seeders/FoodSeeder.php
@@ -9,6 +9,85 @@ class FoodSeeder extends Seeder
 {
     public function run(): void
     {
-        Food::factory()->count(10)->create();
+        $foods = [
+            'Arroz',
+            'Feijão',
+            'Macarrão',
+            'Farinha de mandioca',
+            'Farinha de trigo',
+            'Açúcar',
+            'Sal',
+            'Óleo de soja',
+            'Azeite',
+            'Vinagre',
+
+            'Frango (peito)',
+            'Frango (coxa/sobrecoxa)',
+            'Carne moída',
+            'Acém',
+            'Sardinha em lata',
+            'Atum em lata',
+            'Ovo',
+            'Queijo muçarela',
+            'Queijo minas',
+            'Iogurte natural',
+
+            'Leite integral',
+            'Leite desnatado',
+            'Requeijão',
+            'Manteiga',
+            'Margarina',
+
+            'Alface',
+            'Tomate',
+            'Cebola',
+            'Alho',
+            'Batata',
+            'Batata-doce',
+            'Mandioca (aipim)',
+            'Abobrinha',
+            'Chuchu',
+            'Beterraba',
+            'Couve',
+            'Brócolis',
+            'Espinafre',
+            'Pepino',
+
+            'Banana',
+            'Maçã',
+            'Laranja',
+            'Mamão',
+            'Melancia',
+            'Abacaxi',
+            'Uva',
+            'Limão',
+
+            'Aveia',
+            'Granola',
+            'Pão francês',
+            'Pão integral',
+            'Tapioca (goma)',
+            'Cuscuz (flocão de milho)',
+
+            'Café',
+            'Chá',
+            'Achocolatado em pó',
+            'Mel',
+
+            'Amendoim',
+            'Pasta de amendoim',
+
+            'Molho de tomate',
+            'Milho (lata)',
+            'Ervilha (lata)',
+            'Atum ralado',
+            'Salsicha',
+        ];
+
+        foreach ($foods as $food) {
+            Food::create([
+                'name' => $food,
+            ]);
+        }
     }
 }
